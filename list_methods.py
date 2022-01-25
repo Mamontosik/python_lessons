162669f5ce39bdc6006a438256bf30f4f51f8ac3
Mamontosik <mamontosik@gmail.com>
2022-01-25 18:43:31 +0300

    Изучение списков

---
+++ list_methods.py
@@ -0,0 +1,23 @@
+friends = ['Максим', 'Леонид', 'Екатерина']
+print(friends
+print('Количество друзей', len(friends))
+
+friends.append('Роберт')
+print(friends)
+print('Количество друзей', len(friends))
+
+print(friends.pop())
+print(friends)
+print('Осталось друзей', len(friends))
+
+friends.clear()
+print(friends)
+
+friends = ['Максим', 'Леонид', 'Екатерина']
+print(friends)
+
+friends.remove('Леонид')
+print(friends)
+
+del friends[0]
+print(friends )
